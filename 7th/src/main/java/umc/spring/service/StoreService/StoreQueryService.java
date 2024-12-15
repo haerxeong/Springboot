203cb4bb9779a790bc8fec99838d352b0f87cd6a
haerxeong <haeryeong04@naver.com>
2024-12-15 19:27:23 +0900

    ch9 workbook

--- 7th/src/main/java/umc/spring/service/StoreService/StoreQueryService.java
+++ 7th/src/main/java/umc/spring/service/StoreService/StoreQueryService.java
@@ -1,4 +1,9 @@
 package umc.spring.service.StoreService;
 
+import org.springframework.data.domain.Page;
+import umc.spring.domain.Review;
+
 public interface StoreQueryService {
+    void getReviewList(Long storeId, Integer page);
+    Page<Review> getReviewList(Long StoreId, Integer page);
 }
--- 7th/src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
+++ 7th/src/main/java/umc/spring/service/StoreService/StoreQueryServiceImpl.java
@@ -1,4 +1,30 @@
 package umc.spring.service.StoreService;
 
-public class StoreQueryServiceImpl {
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import umc.spring.domain.Review;
+import umc.spring.domain.Store;
+import umc.spring.repository.ReviewRepository;
+import umc.spring.repository.StoreRepository;
+
+@Service
+@RequiredArgsConstructor
+public class StoreQueryServiceImpl implements StoreQueryService {
+    private final StoreRepository storeRepository;
+    private final ReviewRepository reviewRepository;
+
+    @Override
+    public void getReviewList(Long storeId, Integer page) {
+        Store store = storeRepository.findById(storeId).get();
+
+        Page<Review> StorePage = reviewRepository.findAllByStore(store, PageRequest.of(page, 10));
+        return StorePage;
+    }
+
+    @Override
+    public Page<Review> getReviewList(Long StoreId, Integer page) {
+        return null;
+    }
 }
